c323f158edf899eaa50bfd9b883403885b582b9f
samc <92285153+Samclaxton@users.noreply.github.com>
2022-03-21 16:27:43 +1300

    added navbar to the search function in settings.js

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -4,9 +4,31 @@ import Fuse from 'fuse.js';
 import Item from './item';
 import './styles.css';
 import navbarMain from './navbarMain';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 
+import {
+  Container,
+  Navbar,
+  Button,
+  Dropdown,
+  ButtonGroup,
+  Form,
+  FormControl,
+  NavDropdown,
+  Nav,
+} from 'react-bootstrap';
+import PetLogo from './images/pet-logo.png';
+import DogIcon from './images/dog-icon.png';
+import CatIcon from './images/cat-icon.png';
+import FishIcon from './images/fish-icon.png';
+import TurtleIcon from './images/turtle-icon.png';
+import BirdIcon from './images/bird-icon.png';
+import HorseIcon from './images/horse-icon.png';
+import SidebarFilter from './images/sidebar-filter.png';
+import Card from 'react-bootstrap/Card';
+import ProductImage from './images/Products.png';
+import AnimalNames from './images/animal-names.png';
+import './about.css';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 const SettingsPage = () => {
   const [searchData, setSearchData] = useState(data);
   const searchItem = (query) => {
@@ -30,7 +52,74 @@ const SettingsPage = () => {
   };
   return (
     <div>
-      <navbarMain />
+      <Navbar className="black-navbar" bg="dark" variant="dark">
+        <Container></Container>
+      </Navbar>
+
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <div>
+              <img
+                alt=""
+                src={PetLogo}
+                width="55"
+                height="57"
+                className="pet-logo"
+              />
+              {''}
+            </div>
+            <div class="animal-images">
+              <img
+                alt=""
+                src={DogIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={CatIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={FishIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={BirdIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={TurtleIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />
+              {''}
+              <img
+                alt=""
+                src={HorseIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+            </div>
+          </Navbar.Brand>
+        </Container>
+        <div>
+          <button class="login-btn">Login</button>
+        </div>
+      </Navbar>
       <p className="title"> Search Food</p>
       <div className="search-container">
         <input
