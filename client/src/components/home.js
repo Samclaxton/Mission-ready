50777f38110e4b1b77d25bc9462b2445de479c8e
samc <92285153+Samclaxton@users.noreply.github.com>
2022-03-22 09:28:15 +1300

    added navbar to the home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,112 @@
 import React from 'react';
+import { Navbar, Form, FormControl, Button, Container } from 'react-bootstrap';
 
-const Home = () => {
+import PetLogo from './images/pet-logo.png';
+import DogIcon from './images/dog-icon.png';
+import CatIcon from './images/cat-icon.png';
+import FishIcon from './images/fish-icon.png';
+import TurtleIcon from './images/turtle-icon.png';
+import BirdIcon from './images/bird-icon.png';
+import HorseIcon from './images/horse-icon.png';
+import SidebarFilter from './images/sidebar-filter.png';
+import Card from 'react-bootstrap/Card';
+import ProductImage from './images/main-results.png';
+import AnimalNames from './images/animal-names.png';
+import './about.css';
+
+export default function home() {
   return (
-    <div>
-      <p>Home</p>
+    <div class="first">
+      <Navbar className="black-navbar" bg="dark" variant="dark">
+        <Container></Container>
+      </Navbar>
+
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/">
+            <div>
+              <img
+                alt=""
+                src={PetLogo}
+                width="55"
+                height="57"
+                className="pet-logo"
+              />
+              {''}
+            </div>
+            <div class="animal-images">
+              <img
+                alt=""
+                src={DogIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={CatIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={FishIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={BirdIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+              <img
+                alt=""
+                src={TurtleIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />
+              {''}
+              <img
+                alt=""
+                src={HorseIcon}
+                width="30"
+                height="30"
+                className="dog-image"
+              />{' '}
+            </div>
+          </Navbar.Brand>
+        </Container>
+        <div>
+          <button class="login-btn">Cart</button>
+          <button class="login-btn">Login</button>
+        </div>
+      </Navbar>
+      {/* <img className="animal-names" src={AnimalNames}></img> */}
+      <p></p>
+      <div classname="search-section">
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">Hi Papa Bear!</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
--- client/src/components/item.js
+++ client/src/components/item.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Item = (props) => {
   return (
     <div className="item">
-      <div className="logo">
-        <img src={props.logo} alt={props.name} />
-      </div>
+      <div className="logo"></div>
 
       <div className="name">
         <p>{props.name}</p>
+        <img src={props.logo} alt={props.name} />
+        <p className="price">{props.price}</p>
+        <p className="thumbnail-text">{props.text}</p>
         <div className="link">
           <p>{props.link}</p>
         </div>
